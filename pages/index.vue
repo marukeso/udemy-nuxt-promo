6a818485dc98f9ebd74ed2d232d71b1dab33d4ab
Kentaro Maruyama <marukeso@gmail.com>
2021-07-22 10:19:02 +0900

    Test on index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -21,13 +21,13 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
+import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
     Logo,
   },
-};
+}
 </script>
 
 <style>
@@ -41,8 +41,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
